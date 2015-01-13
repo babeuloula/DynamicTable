8f04927635755df7e5313da0b6e1dcf415b637f1
BaBeuloula <babeuloula@gmail.com>
2015-01-13 14:06:56 +0100

    modifications diverses
    - create() : Changement de la syntaxe (regarder le Readme)
    - getRows() : Ajout de toutes les colonnes (y compris les commentaires)
    - addRow(), renameRow() : Possibilité de rajouter des commentaires
    - replaceChars() : Sur toutes les fonctions, évite d'avoir des tables avec des espaces et des caractères spéciaux

--- DynamicTable.php
+++ DynamicTable.php
@@ -14,25 +14,30 @@
          * @param PDO $pdo instance d'une connexion mysql via PDO
          */
         public function __construct($table, $pdo) {
-            $this->table = $table;
+            $this->table = $this->replaceChars($table);
             $this->db = $pdo;
         }
 
 
         /**
          * Permet de créer la table
-         * @param array $rows contenu de la table (colonne => type)
+         * @param array $rows contenu de la table array('title' => titre, 'type' => type, 'comment' => commentaire)
          *
          * @return DynamicTable
          */
         public function create($rows) {
             if(!is_array($rows)) {
-                throw new Exception("Vous devez rentrer les colonnes à créer sous forme d'un tableau associatif.");
+                throw new Exception("Vous devez rentrer les colonnes à créer sous forme d'un tableau.");
             }
 
-            $create_sql = "CREATE TABLE IF NOT EXISTS `" . $this->table . "` ( `id` int(11) NOT NULL AUTO_INCREMENT,";
-            foreach($rows as $row => $type) {
-                $create_sql.= "`" . $row . "` " . $this->getRowType($type) . " NOT NULL,";
+            $create_sql = "CREATE TABLE IF NOT EXISTS `" . $this->table . "` ( `id` int(11) NOT NULL AUTO_INCREMENT, ";
+            foreach($rows as $row) {
+                if(!isset($row['title']) || !isset($row['type'])) {
+                    throw new Exception("Vous devez spécifier le nom de la colonne et son type.");
+                } else {
+                    $comment = (isset($row['comment'])) ? " COMMENT '" . addslashes($row['comment']) . "'" : "";
+                    $create_sql.= "`" . $this->replaceChars($row['title']) . "` " . $this->getRowType($row['type']) . " NOT NULL" . $comment . ",";
+                }
             }
             $create_sql.= "PRIMARY KEY (`id`)) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1";
 
@@ -52,7 +57,7 @@
          */
         public function getRows() {
             try {
-                $recordset = $this->db->query("SHOW COLUMNS FROM `" . $this->table . "`");
+                $recordset = $this->db->query("SHOW FULL COLUMNS FROM `" . $this->table . "`");
                 return $recordset->fetchAll(PDO::FETCH_ASSOC);
             } catch (PDOException $e) {
                 die($e->getMessage());
@@ -66,26 +71,29 @@
          * @param string $type type de la colonne
          * @param string $order emplacement de la table (first, after ou end)
          * @param string $after après quelle colonne insérer la colonne (uniquement si after)
+         * @param string $comment commentaire de la colonne
          *
          * @return DynamicTable
          */
-        public function addRow($name, $type, $order = 'end', $after = null) {
+        public function addRow($name, $type, $order = 'end', $after = null, $comment = null) {
+            $comment = ($comment !== null) ? " COMMENT '" . addslashes($comment) . "'" : "";
+
             try {
                 switch ($order) {
                     case 'first':
-                        $this->db->exec("ALTER TABLE `" . $this->table . "` ADD `" . $name . "` " . $this->getRowType($type) . " NOT NULL FIRST");
+                        $this->db->exec("ALTER TABLE `" . $this->table . "` ADD `" . $this->replaceChars($name) . "` " . $this->getRowType($type) . " NOT NULL " . $comment . " FIRST");
                         break;
 
                     case 'after':
                         if(!$after !== null) {
-                            throw new Exception("Vous devez indiquer après quelle colonne insérer " . $name);
+                            throw new Exception("Vous devez indiquer après quelle colonne insérer " . $this->replaceChars($name));
                         } else {
-                            $this->db->exec("ALTER TABLE `" . $this->table . "` ADD `" . $name . "` " . $this->getRowType($type) . " NOT NULL AFTER `" . $after . "`");
+                            $this->db->exec("ALTER TABLE `" . $this->table . "` ADD `" . $this->replaceChars($name) . "` " . $this->getRowType($type) . " NOT NULL " . $comment . " AFTER `" . $this->replaceChars($after) . "`");
                         }
                         break;
 
                     default:
-                        $this->db->exec("ALTER TABLE `" . $this->table . "` ADD `" . $name . "` " . $this->getRowType($type) . " NOT NULL");
+                        $this->db->exec("ALTER TABLE `" . $this->table . "` ADD `" . $this->replaceChars($name) . "` " . $this->getRowType($type) . " NOT NULL " . $comment);
                         break;
                 }
 
@@ -101,10 +109,11 @@
          * @param string $oldName ancien nom de la table
          * @param string $newName nouveau nom de la table
          * @param string $type nouveau type de la table
+         * @param string $comment commentaire de la colonne
          *
          * @return DynamicTable
          */
-        public function renameRow($oldName, $newName, $type = null) {
+        public function renameRow($oldName, $newName, $type = null, $comment = null) {
             if($type !== null) {
                 foreach($this->getRows() as $row) {
                     if($row['Field'] === $oldName) {
@@ -113,7 +122,8 @@
                 }
             }
 
-            $this->db->exec("ALTER TABLE `" . $this->table . "` CHANGE `" . $oldName . "` `" . $newName . "` " . $this->getRowType($type) . " NOT NULL");
+            $comment = ($comment !== null) ? " COMMENT '" . addslashes($comment) . "'" : "";
+            $this->db->exec("ALTER TABLE `" . $this->table . "` CHANGE `" . $this->replaceChars($oldName) . "` `" . $this->replaceChars($newName) . "` " . $this->getRowType($type) . " NOT NULL " . $comment);
 
             return new DynamicTable($this->table, $this->db);
         }
@@ -133,7 +143,7 @@
                 }
             }
 
-            $this->db->exec("ALTER TABLE `" . $this->table . "` CHANGE `" . $rowName . "` `" . $rowName . "` " . $this->getRowType($type) . " NOT NULL AFTER `" . $after . "`");
+            $this->db->exec("ALTER TABLE `" . $this->table . "` CHANGE `" . $this->replaceChars($rowName) . "` `" . $this->replaceChars($rowName) . "` " . $this->getRowType($type) . " NOT NULL AFTER `" . $this->replaceChars($after) . "`");
 
             return new DynamicTable($this->table, $this->db);
         }
@@ -146,7 +156,7 @@
          * @return DynamicTable
          */
         public function deleteRow($name) {
-            $this->db->exec("ALTER TABLE `" . $this->table . "` DROP `" . $name . "`");
+            $this->db->exec("ALTER TABLE `" . $this->table . "` DROP `" . $this->replaceChars($name) . "`");
 
             return new DynamicTable($this->table, $this->db);
         }
@@ -168,8 +178,8 @@
          * @return DynamicTable
          */
         public function renameTable($newName) {
-            $this->db->exec("ALTER TABLE `" . $this->table . "` RENAME `" . $newName . "`");
-            $this->table = $newName;
+            $this->db->exec("ALTER TABLE `" . $this->table . "` RENAME `" . $this->replaceChars($newName) . "`");
+            $this->table = $this->replaceChars($newName);
 
             return new DynamicTable($this->table, $this->db);
         }
@@ -236,5 +246,19 @@
                     throw new Exception("Ce champs n'est pas encore prévu dans la classe.");
                     break;
             }
+        }
+
+
+        /**
+         * Permet de prévenir les espaces et les accents dans le nom des colonnes
+         * @params string $string nom de la colonne
+         *
+         * @return string le nom de la colonne sans les caractères spéciaux
+         */
+        private function replaceChars($string) {
+            $in  = array('’',':',';',',',' ',"'",'"','&','~','(',')','{','#','[','|','`','\\','^','@',']','}','¤','%','§','/','À','Á','Â','Ã','Ä','Å','à','á','â','ã','ä','å','Ò','Ó','Ô','Õ','Ö','Ø','ò','ó','ô','õ','ö','ø','È','É','Ê','Ë','è','é','ê','ë','Ç','ç','Ì','Í','Î','Ï','ì','í','î','ï','Ù','Ú','Û','Ü','ù','ú','û','ü','ÿ','Ñ','ñ','À','Á','Â','Ã','Ä','Å','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ò','Ó','Ô','Õ','Ö','Ù','Ú','Û','Ü','Ý','à','á','â','ã','ä','å','ç','è','é','ê','ë','ì','í','î','ï','ð','ò','ó','ô','õ','ö','ù','ú','û','ü','ý','ÿ','+','–','.','€');
+            $out = array('','','','','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','a','a','a','a','a','a','a','a','a','a','a','a','o','o','o','o','o','o','o','o','o','o','o','o','e','e','e','e','e','e','e','e','c','c','i','i','i','i','i','i','i','i','u','u','u','u','u','u','u','u','y','n','n','A','A','A','A','A','A','C','E','E','E','E','I','I','I','I','O','O','O','O','O','U','U','U','U','Y','a','a','a','a','a','a','c','e','e','e','e','i','i','i','i','o','o','o','o','o','o','u','u','u','u','y','y','-','-','','e');
+
+            return strtolower(str_replace($in, $out, $string));
         }
     }
